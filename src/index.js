00277ed014eb651aac6c43b38374169f44079e53
Benjamin Williams <bw15@staff.staffs.ac.uk>
2023-12-05 15:42:31 +0000

    added react boilerplate

--- src/index.js
+++ src/index.js
@@ -1,26 +1,13 @@
-// Test import of a JavaScript module
-import { example } from '@/js/example'
+//
+import '@/styles/index.sass'
 
-// Test import of an asset
-import webpackLogo from '@/images/webpack-logo.svg'
+//Import react
+import React from 'react';
+import { createRoot } from "react-dom/client";
 
-// Test import of styles
-import '@/styles/index.scss'
 
-// Appending to the DOM
-const logo = document.createElement('img')
-logo.src = webpackLogo
+//----------
 
-const heading = document.createElement('h1')
-heading.textContent = example()
-
-// Test a background image url in CSS
-const imageBackground = document.createElement('div')
-imageBackground.classList.add('image')
-
-// Test a public folder asset
-const imagePublic = document.createElement('img')
-imagePublic.src = '/assets/example.png'
-
-const app = document.querySelector('#root')
-app.append(logo, heading, imageBackground, imagePublic)
+//Create root
+const root = createRoot(document.getElementById("root"));
+root.render(<h1>It's alive!</h1>);
